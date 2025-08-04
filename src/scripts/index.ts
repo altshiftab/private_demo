59e4468d9312c4115397c7fe52a62154f7182940
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2025-08-04 13:51:46 +0200

    Initial

---
+++ src/scripts/index.ts
@@ -0,0 +1,7 @@
+import "@altshiftab/styles/common.css";
+import {addErrorEventListeners} from "@altshiftab/http_service_utils_js";
+import "@altshiftab/web_components/button";
+
+import "../styles/index.css";
+
+addErrorEventListeners();
