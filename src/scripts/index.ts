a5191e411e85ffc95fc7995235adea99043807df
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2025-08-04 14:15:36 +0200

    Working

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,7 +1,11 @@
 import "@altshiftab/styles/common.css";
-import {addErrorEventListeners} from "@altshiftab/http_service_utils_js";
+import "@altshiftab/styles/common_header_footer.css";
+import {addErrorEventListeners, refreshSession} from "@altshiftab/http_service_utils_js";
 import "@altshiftab/web_components/button";
+import "@altshiftab/web_components/footer"
+import "@altshiftab/web_components/header"
 
 import "../styles/index.css";
 
 addErrorEventListeners();
+refreshSession(new URL("https://login.alt-shift.se/api/refresh-session-token"), new URL("https://login.alt-shift.se/"));
